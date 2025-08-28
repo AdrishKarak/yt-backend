7932d7a9e68a46dc162db97d768c3a173e82cd1c
Adrish <runakarak@gmail.com>
2025-08-29 03:11:07 +0530

    api response and error handling and utils discusiion

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ dotenv.config({
     path: './.env'
 })
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.ev.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`)
+    })
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection failed",err)
+  })
 
 
 
